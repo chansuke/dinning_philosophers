24e43eafba6ce514233433fdadd9832ccf10b95a
chansuke <chansuke@georepublic.co.jp>
2017-03-17 22:53:01 +0900

    Add philosophers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Philosopher {
+    name: String,
+}
+
+fn Philosopher() {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Philosopher::new("Judith Butler");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Max");
+    let p4 = Philosopher::new("Emma Goldman");
+    let p5 = Philosopher::new("Michel Foucault");
 }
